bc044e7a9f9c8b9289d4bea9f100c2e564f8ab75
Shawnsdaddy <wu2xx@dukes.jmu.edu>
2024-05-15 09:58:08 -0700

    59

--- 59_three_sum_closest.py
+++ 59_three_sum_closest.py
@@ -3,11 +3,12 @@ from typing import (
 )
 
 
-def three_sum(self, numbers: List[int], target: int) -> int:
+def three_sum(numbers: List[int], target: int) -> int:
     # write your code here
     numbers.sort()
     n = len(numbers)
-    closest = float('infi')
+    closest = float('inf')
+    res = float('inf')
     for i in range(n - 2):
         if i > 0 and numbers[i] == numbers[i - 1]:
             continue
@@ -17,16 +18,33 @@ def three_sum(self, numbers: List[int], target: int) -> int:
             total = numbers[i] + numbers[left_index] + numbers[right_index]
             diff = total - target
             if diff == 0:
-                return 0
+                return total
             elif diff < 0:
+                left_index += 1
                 diff = 0 - diff
+            else:
+                right_index -= 1
             if diff < closest:
                 closest = diff
-                res.append([numbers[i], numbers[left_index], numbers[right_index]])
-                while left_index + 1 < n - 1 and numbers[left_index] == numbers[left_index + 1]:
-                    left_index += 1
-                while right_index - 1 > 0 and numbers[right_index] == numbers[right_index - 1]:
-                    right_index -= 1
-                left_index += 1
-                right_index -= 1
+                res = total
     return res
+
+
+print(three_sum([1, 0, -1, -1, -1, -1, 0, 1, 1, 1, 2], 7))
+
+
+def threeSumClosest(numbers, target):
+    numbers.sort()
+    ans = None
+    for i in range(len(numbers)):
+        left, right = i + 1, len(numbers) - 1
+        while left < right:
+            sum = numbers[left] + numbers[right] + numbers[i]
+            if ans is None or abs(sum - target) < abs(ans - target):
+                ans = sum
+
+            if sum <= target:
+                left += 1
+            else:
+                right -= 1
+    return ans
