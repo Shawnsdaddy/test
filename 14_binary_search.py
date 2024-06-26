c8fe5b060aebeca1fbc94ffcc967a300126680ea
Xubo Wu <frankylwolf@Xubos-MBP.lan>
2024-06-26 10:45:59 -0700

    solution 2& 3

--- 14_binary_search.py
+++ 14_binary_search.py
@@ -37,7 +37,37 @@ def binary_search(nums: List[int], target: int) -> int:
         return right
     else :
         return  - 1
+    
+def binary_search_2(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) -1
+    while left <= right:
+        mid = left + (right - left)//2
+        if nums[mid] < target:
+            left = mid+1
+        else:
+            right = mid-1
+    print(left,right)
+    if right >= 0 and nums[right] == target:
+        return right
+    if left <= len(nums)-1 and nums[left] == target:
+        return left
+    return -1
+
+def binary_search_3(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) -1
+    while left < right:
+        mid = left + (right - left)//2
+        if nums[mid] < target:
+            left = mid+1
+        else:
+            right = mid
+    print(left,right)
+    if right >= 0 and nums[right] == target:
+        return right
+    return -1
 
 
 
-print(binary_search([3, 4, 5, 8, 8, 8, 8, 10, 13, 14], 8))
+print(binary_search_3([3, 4, 5, 8, 8, 8, 8, 10, 13, 14], 8))
